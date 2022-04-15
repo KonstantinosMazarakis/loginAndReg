40054f4e12b4cc4371a2928306ab50ab625d9767
konstantinos mazarakis <kostasbusiness@gmail.com>
2022-04-14 21:47:59 -0400

    commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using loginAndReg.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+
+namespace loginAndReg.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MyContext _context;
+
+        public HomeController(ILogger<HomeController> logger, MyContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost("/users/create")]
+        public IActionResult createUser(User newUser)
+        {
+            if(ModelState.IsValid)
+            {
+                if(_context.Users.Any(s => s.Email == newUser.Email))
+                {
+                    ModelState.AddModelError("Email", "Email already in use!");
+                    return View("Index");
+                }
+                PasswordHasher<User> Hasher = new PasswordHasher<User>();
+                newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+                _context.Add(newUser);
+                _context.SaveChanges();
+                HttpContext.Session.SetInt32("UserId", newUser.UserId);
+                return RedirectToAction("success");
+            }else
+            {
+                return View("Index");
+            }
+        }
+
+
+        [HttpGet("/login")]
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+
+        [HttpPost("/users/login")]
+        public IActionResult  PostLogin(LoginUser loginUser)
+        {
+            if(ModelState.IsValid)
+            {
+                User userInDb = _context.Users.FirstOrDefault(d => d.Email == loginUser.Email);
+                if(userInDb == null)
+                {
+                    ModelState.AddModelError("Email","Invalid Email/Password");
+                    return View("Login");
+                }
+                PasswordHasher<LoginUser> hasher = new PasswordHasher<LoginUser>();
+                var result = hasher.VerifyHashedPassword(loginUser, userInDb.Password,loginUser.Password);
+                if(result == 0)
+                {
+                    ModelState.AddModelError("Password","Invalid Email/Password");
+                    return View("Login");
+                }
+                HttpContext.Session.SetInt32("UserId", userInDb.UserId);
+                return RedirectToAction("Success");
+            }else
+            {
+                return View("Login");
+            }
+        }
+
+        [HttpGet("/success")]
+        public IActionResult Success()
+        {
+            if(HttpContext.Session.GetInt32("UserId") == null)
+            {
+                return RedirectToAction("Index");
+            }
+            User LoggedInUser = _context.Users.FirstOrDefault(a => a.UserId == (int)HttpContext.Session.GetInt32("UserId"));
+            
+            return View("Success");
+        }
+
+        [HttpPost("/users/logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,32 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace loginAndReg.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId {get;set;}
+        [Required]
+        [MinLength(2)]
+        public string FirstName {get;set;}
+        [Required]
+        [MinLength(2)]
+        public string LastName {get;set;}
+        [Required]
+        [EmailAddress]
+        public string Email {get;set;}
+        [Required]
+        [MinLength(8)]
+        [DataType(DataType.Password)]
+        public string Password {get;set;}
+        public DateTime CreatedAt {get;set;} = DateTime.Now;
+        public DateTime UpdatedAt {get;set;} = DateTime.Now;
+        [NotMapped]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword {get;set;}
+
+    }
+}
